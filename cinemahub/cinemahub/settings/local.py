018d46c8b67d0f80618950a572b46865eb0b4260
Darshik <31007901+d4rshik@users.noreply.github.com>
2018-10-20 21:01:01 +0530

    Update local.py

--- cinemahub/cinemahub/settings/local.py
+++ cinemahub/cinemahub/settings/local.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y1=%lw86@getmn^7zw91%&639ri*_o43+1fpv4o)e70h8%ac!9'
+SECRET_KEY = '***************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
